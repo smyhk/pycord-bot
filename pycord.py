187ca5bb6c0ac8c047b6e98355b737217518aead
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-01-22 22:28:10 -0800

    Clean up code

--- pycord.py
+++ pycord.py
@@ -18,17 +18,25 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
-    if message.content == "cookie":
+    elif message.content == "cookie":
         await client.send_message(message.channel, ":cookie:")
-
-    if message.content.upper().startswith("!PING"):
+    elif message.content.upper().startswith("!PING"):
         msg = "{0.author.mention} Pong!".format(message)
         await client.send_message(message.channel, msg)
-
-    if message.content.upper().startswith("!SAY"):
-        args = message.content.split(" ")
-        await client.send_message(message.channel, "%s" % " ".join(args[1:]))
+    elif message.content.upper().startswith("!SAY"):
+        # only user with this id can execute the command
+        if message.author.id == "351094569309306891":
+            args = message.content.split(" ")
+            msg = " ".join(args[1:]).format(message)
+            await client.send_message(message.channel, msg)
+        else:
+            await client.send_message(message.channel, "Ah ah ah, you didn't say the magic word!")
+    elif message.content.upper().startswith("!AMIADMIN"):
+        # user users with this role id can execute the command
+        if "405241489292263424" in [role.id for role in message.author.roles]:
+            await client.send_message(message.channel, "You are an admin")
+        else:
+            await client.send_message(message.channel, "You are not an admin")
 
 
 # Load the bot token from an environment variable
